bf6c9a9b79fadac8275f21dc4c92dc09e1aa62e3
agpsl <agnaldo.silva.mg@gmail.com>
2024-05-31 19:49:41 -0300

    usa tema escuro por padrao

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 function App() {
   return (
     <div className="App">
-      <Header />
-      <ListarTarefa />
+      <ThemeProvider theme={darkTheme}>
+        <Header />
+        <ListarTarefa />
+      </ThemeProvider>
     </div>
   );
 }
